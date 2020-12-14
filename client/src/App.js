9f0e83409adaf78cbbd06056fdc02554cb37042d
Simmigon <simmigonflagg@gmail.com>
2020-12-13 21:42:51 -0500

    Working Demo

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,77 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import MovieList from "./components/MovieList";
-import MovieHeader from './components/MovieHeader';
-import SearchBox from './components/SearchBox';
+import MovieHeader from "./components/MovieHeader";
+import SearchBox from "./components/SearchBox";
+import AddFavMovieComponent from "./components/AddFavorMovie";
+import RemoveFavMovieComponent from './components/RemoveFavorMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchMovie, setSearchMovie] = useState('');
+  const [favorites, setFavorites] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
 
-  const getMovies = async () =>{
+  const getMovies = async () => {
     const url = `http://www.omdbapi.com/?s=${searchMovie}&apikey=${process.env.REACT_APP_OMDB_API}`;
     const response = await fetch(url);
-    const responseJSON = await response.json()
-    console.log(responseJSON);
-    
-    if(responseJSON.Search){
+    const responseJSON = await response.json();
+   
+
+    if (responseJSON.Search) {
       setMovies(responseJSON.Search);
     }
-  }
+  };
 
   //Run this when the page loads
   useEffect(() => {
     getMovies();
-  }, [searchMovie])
+  }, [searchMovie]);
+
+  //Run Check to see if data is in local storage
+  useEffect(() => {
+    const movie_data = JSON.parse(localStorage.getItem('movie-data'))
+    setFavorites(movie_data);
+  }, []);
+
+  const saveUserDataToLocal = (movieData) =>{
+    localStorage.setItem('movie-data', JSON.stringify(movieData))
+  };
+  // Remove All
+  const removeAllUserDataToLocal = () =>{
+    localStorage.removeItem('movie-data')
+  };
+  const addFavoriteMovie = (movie) => {
+    const newFavList = [...favorites, movie];
+    setFavorites(newFavList);
+    saveUserDataToLocal(newFavList);
+  };
+  const removeFavoriteMovie = (movie) => {
+    const removedOneMovie = favorites.filter((favorite) => favorite.imdbID !== movie.imdbID)
+    setFavorites(removedOneMovie);
+    saveUserDataToLocal(removedOneMovie)
+  };
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
-      <MovieHeader heading="Movies"/>
-      <SearchBox searchMovie={searchMovie} setSearchMovie={setSearchMovie}/>
+        <MovieHeader heading="Movies" />
+        <SearchBox searchMovie={searchMovie} setSearchMovie={setSearchMovie} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavOnClickFromApp={addFavoriteMovie}
+          favoriteComp={AddFavMovieComponent}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieHeader heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavOnClickFromApp={removeFavoriteMovie}
+          favoriteComp={RemoveFavMovieComponent}
+        />
       </div>
     </div>
   );
--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,9 +1,13 @@
 const MovieList = (props) => {
+  const FavoriteComp = props.favoriteComp;
   return (
     <>
       {props.movies.map((movie, key) => (
-        <div>
-          <img src={movie.Poster} />
+        <div className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="Movie" />
+          <div onClick={() => props.handleFavOnClickFromApp(movie)} className="overlay d-flex align-items-center justify-content">
+            <FavoriteComp />
+          </div>
         </div>
       ))}
     </>
